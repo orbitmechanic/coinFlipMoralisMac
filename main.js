dec8c900b49c964ed4fb30b82b4942cd5543889d
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-03-18 17:44:28 -0700

    Switch display on login.

--- main.js
+++ main.js
@@ -5,10 +5,18 @@ async function login() {
     try {
         user = await Moralis.Web3.authenticate();
         console.log(user);
-        alert("User logged in")
+        alert("User logged in");
+        document.getElementById("login_button").style.display = "none";
+        document.getElementById("game").style.display = "block";
     } catch (error) {
         console.log(error);
     }
 }
 
+async function flip(side) {
+    alert(side);
+}
+
 document.getElementById("login_button").onclick = login;
+document.getElementById("headsbutton").onclick = function(){flip("heads")};
+document.getElementById("tailsbutton").onclick = function(){flip("tails")};
